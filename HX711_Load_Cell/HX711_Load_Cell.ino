b3ebce5b49e8e64bbcb1587f6ee90c99fa173362
MryNightmare <40217025+MryNightmare@users.noreply.github.com>
2021-11-15 18:12:34 -0800

    added highest number record function
    -button 2 to start record function, button 3 to stop
    -when the function is enabled: LED1 will blink and the screen will display the greatest weight until disabled

---
+++ HX711_Load_Cell/HX711_Load_Cell.ino
@@ -0,0 +1,87 @@
+#include <HX711.h>
+#include <TimerOne.h>
+#include <Wire.h>
+#include <MultiFuncShield.h>
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 5;
+const int LOADCELL_SCK_PIN = 6;
+
+HX711 scale;
+
+int highest_num = 0;
+int curr_num = 0;
+bool f_record = false;
+byte btn;
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.println("MFS Setup");
+    Timer1.initialize();
+    MFS.initialize(&Timer1);
+
+    Serial.println("HX711 Setup");
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+    scale.set_scale(100469); 
+    scale.tare();
+}
+
+void loop() {
+    doButton();
+    doScale();
+    doLogic();
+
+    delay(100);
+}
+
+void doButton() {
+    btn = MFS.getButton();
+}
+
+void doScale() {
+    Serial.print("Weight: ");
+    Serial.println(scale.get_units());
+
+    curr_num = scale.get_units();
+}
+
+void doLogic() {
+    if (btn) {
+        byte buttonNumber = btn & B00111111;
+        byte buttonAction = btn & B11000000;
+
+        if (buttonAction == BUTTON_PRESSED_IND) {
+            if (buttonNumber == 1) {
+                    scale.tare();
+                    Serial.println("Scale Tared");
+            } else if (buttonNumber == 2) {
+                // Record Mode
+                if (!f_record) {
+                    f_record = true;
+                    MFS.writeLeds(LED_1, ON);
+                    MFS.blinkLeds(LED_1, ON);
+                }
+            } else if (buttonNumber == 3) {
+                if (f_record) {
+                    f_record = false;
+                    highest_num = 0;
+                    MFS.blinkLeds(LED_1, OFF);
+                    MFS.writeLeds(LED_1, OFF);
+                }
+            }
+        }
+    }
+
+    if (f_record) {
+        if (curr_num > highest_num) {
+            highest_num = curr_num;
+        }
+        Serial.print("Highest Weight: ");
+        Serial.println(scale.get_units());
+        MFS.write(highest_num);
+    } else {
+        MFS.write(curr_num);
+    }
+}
